d31ca524d7d30568f7a1f534ea257cd009964967
Shailendra <shailendrakmr471@gmail.com>
2020-04-22 00:05:17 +0530

    BMI Calculator Private Properties

---
+++ force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -0,0 +1,28 @@
+import { LightningElement, track } from 'lwc';
+
+export default class BmiCalculator extends LightningElement {
+    // Private Properties are non-reactive in nature, which means they don't create data binding and doesn't 
+    // update template on their value change
+    cardTitle = 'BMI Calculator';
+
+    changePrivatePropertyHandler(){
+        this.cardTitle = 'Changed Value';
+        console.log('Value : ',this.cardTitle);
+    }
+
+    // Private Properties can be made reactive by using @track decorator which develops data binding behind 
+    // the scene and update the template on their value change
+    weight;
+    height;
+    @track bmi;
+    onWeightChange(event){       // inputbox returns string value thats why we need to parse it into Float value
+        this.weight = parseFloat(event.target.value);
+    }
+    onHeightChange(event){
+        this.height = parseFloat(event.target.value);
+    }
+    calculateBMI(){
+        this.bmi = this.weight / (this.weight + this.height);
+    }
+
+}
