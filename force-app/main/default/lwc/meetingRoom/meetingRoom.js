277b02edf7d067c5fac318fc3133517b7b36d999
Shailendra <shailendrakmr471@gmail.com>
2020-04-23 01:25:11 +0530

    Meeting Rooms using public properties

---
+++ force-app/main/default/lwc/meetingRoom/meetingRoom.js
@@ -0,0 +1,15 @@
+import { LightningElement, api } from 'lwc';
+
+export default class MeetingRoom extends LightningElement {
+
+// public properties are reactive. Updating a new value in parent component, automatically updates child component.
+// Use public properties whenever you need to pass values from one component to another.
+
+    @api meetingRoomInfo;  //{roomName:'A-01', roomCapacity:'10'}
+
+// Use @api decorator to define public properties in your component. Values of these properties can be 
+// supplied by parent component but you can also set default value
+// e.g. Here we can set default value for 'meetingRoomInfo' and can not modify it later on. 
+// These values can come from parent component(meetingRooms).
+
+}
---
+++ force-app/main/default/lwc/meetingRooms/meetingRooms.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class MeetingRooms extends LightningElement {
+    meetingRoomInfo = [
+        {roomName:'A-01', roomCapacity:'10'},
+        {roomName:'A-02', roomCapacity:'12'},
+        {roomName:'A-03', roomCapacity:'9'},
+        {roomName:'B-01', roomCapacity:'7'},
+        {roomName:'B-02', roomCapacity:'6'}
+    ];
+}
