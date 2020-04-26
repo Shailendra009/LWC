a3c1db4b1485983fbe4244936da73a720519b227
Shailendra <shailendrakmr471@gmail.com>
2020-04-26 18:27:57 +0530

    Using public method to call method on child component

---
+++ force-app/main/default/lwc/publicMethodParent/publicMethodParent.js
@@ -0,0 +1,17 @@
+import { LightningElement, track } from 'lwc';
+
+export default class PublicMethodParent extends LightningElement {
+    @track value;
+
+    // Public Methods: To call method on child component from parent component
+
+    checkboxSelectHandler(){
+        const childComponent = this.template.querySelector('c-public-method-child');
+        const returnedMessage = childComponent.selectCheckbox(this.value);
+        console.log('Returned Message: ',returnedMessage);
+    }
+
+    onInputChangeHandler(event){
+        this.value = event.target.value;
+    }
+}
