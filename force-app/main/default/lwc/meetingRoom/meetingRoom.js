a9d2ab46cd828f0db6ae4c3434bf0605f44208c6
Shailendra <shailendrakmr471@gmail.com>
2020-04-29 20:29:55 +0530

    Dispatching and Handling Events (Child to Parent)

--- force-app/main/default/lwc/meetingRoom/meetingRoom.js
+++ force-app/main/default/lwc/meetingRoom/meetingRoom.js
@@ -13,5 +13,21 @@ export default class MeetingRoom extends LightningElement {
 // These values can come from parent component(meetingRooms).
 
     @api showRoomInfo = false;
+    tileClickHandler(){
+        // Declare/Register the event
+        const tileClicked = new CustomEvent('tileclick', {detail: this.meetingRoomInfo, bubbles : true});
+        // 'tileclick': Name of event
+        // {detail:''}: Parameter to pass 
+        // bubbles:true   To allow your event to bubbles up to parent component
+
+        // Fire/Dispatch the event
+        this.dispatchEvent(tileClicked);
+        // Event can be handled in two ways
+        // one way is to handle in component tag(markup). Append 'on' keyword before your event name in markup
+        // you can also handle in js file (to handle based on some condition)
+
+        // Custom Events can only be handled by the component itself or the parent component
+        // Custom Event is same as 'Component Event' in Lightning Component 
+    }
 
 }
--- force-app/main/default/lwc/meetingRooms/meetingRooms.js
+++ force-app/main/default/lwc/meetingRooms/meetingRooms.js
@@ -1,6 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class MeetingRooms extends LightningElement {
+    
+    @track selectedMeetingRoom;
+    
     meetingRoomInfo = [
         {roomName:'A-01', roomCapacity:'10'},
         {roomName:'A-02', roomCapacity:'12'},
@@ -8,4 +11,17 @@ export default class MeetingRooms extends LightningElement {
         {roomName:'B-01', roomCapacity:'7'},
         {roomName:'B-02', roomCapacity:'6'}
     ];
+
+    onTileSelectHandler(event){
+        const currentMeetingRoomInfo = event.detail;
+        this.selectedMeetingRoom = currentMeetingRoomInfo.roomName;
+    }
+
+    // To handle event at time of component creation we use constructor
+    // super() keyword should be the 1st statement in constructor
+    constructor(){
+        super();
+        this.template.addEventListener('tileclick', this.onTileSelectHandler.bind(this));
+    }
+
 }
