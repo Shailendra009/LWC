45d69f640eb515330ae0444b9beede60cbc1fbb6
Shailendra <shailendrakmr471@gmail.com>
2020-05-05 01:38:48 +0530

    View Account Record using LDS

--- force-app/main/default/lwc/accountManagerLDS/accountManagerLDS.js
+++ force-app/main/default/lwc/accountManagerLDS/accountManagerLDS.js
@@ -1,13 +1,19 @@
-import { LightningElement, track } from 'lwc';
-import { createRecord } from 'lightning/uiRecordApi';      // here 'lightning' represents namespace and 'uiRecordApi': module
+import { LightningElement, track, wire } from 'lwc';
+import { createRecord, getRecord } from 'lightning/uiRecordApi';      // here 'lightning' represents namespace and 'uiRecordApi': module
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+const fieldArray = ['Account.Name', 'Account.Phone', 'Account.Website'];
 
 export default class AccountManagerLDS extends LightningElement {
     debugger;
     @track accountName;
     @track accountPhone;
     @track accountWebsite;
-    
+
+    @track recordId;
+
+    @wire(getRecord, {recordId : '$recordId', fields : fieldArray}) accountRecord;
+
     accountNameChangeHandler(event){
         this.accountName = event.target.value;
         console.log('accountName : ',this.accountName);
@@ -36,6 +42,7 @@ export default class AccountManagerLDS extends LightningElement {
         // 'Promise Object' represents the eventual completion(or failure) of an asynchronous operation, and its resulting value.
 
         createRecord(recordInput).then(response => {
+            this.recordId = response.id;
             console.log('response : ' + JSON.stringify(response));
             this.dispatchEvent(
                 new ShowToastEvent({
@@ -55,6 +62,27 @@ export default class AccountManagerLDS extends LightningElement {
                 }),
             );
             console.log('Error in creating Account : ',error.body.message);
-        })
+        });
+    }
+
+    get retAccountName(){
+        if(this.accountRecord.data){
+            return this.accountRecord.data.fields.Name.value;
+        }
+        return undefined;
+    }
+
+    get retAccountPhone(){
+        if(this.accountRecord.data){
+            return this.accountRecord.data.fields.Phone.value;
+        }
+        return undefined;
+    }   
+
+    get retAccountWebsite(){
+        if(this.accountRecord.data){
+            return this.accountRecord.data.fields.Website.value;
+        }
+        return undefined;
     }
 }
