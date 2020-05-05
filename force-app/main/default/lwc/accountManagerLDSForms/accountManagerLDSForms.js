3db1f4e1c7ece46f24f210be03c1ed9b515ae22a
Shailendra <shailendrakmr471@gmail.com>
2020-05-05 20:05:27 +0530

    Create and View Account Record using LDS Forms

---
+++ force-app/main/default/lwc/accountManagerLDSForms/accountManagerLDSForms.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class AccountManagerLDSForms extends LightningElement {
+
+    recordId;   // Before Spring'20, we need to import track decorator to make varible(Primitive data types) reactive
+                // After Spring'20 @track decorator can be used for non-primitive e.g. Array, Objects etc.
+
+    successHandler(event){
+        this.recordId = event.detail.id;     // used to get the Id of current record
+    }  
+}
