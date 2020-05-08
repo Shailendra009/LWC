e44ef0b5025c22d360cfa1590dd5f50324f4e8fb
Shailendra <shailendrakmr471@gmail.com>
2020-05-08 20:34:26 +0530

    Calling Apex Method Imperatively and Using Wire Adapter with Toast Notification

--- force-app/main/default/lwc/accountManagerApexCall/accountManagerApexCall.js
+++ force-app/main/default/lwc/accountManagerApexCall/accountManagerApexCall.js
@@ -1,5 +1,6 @@
 import { LightningElement, wire } from 'lwc';
 import getAllAccounts from '@salesforce/apex/AccountManager.getAccounts'; // This needs to be imported to get Apex method called Imperatively or by Wire Adapter
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';  // This needs to be imported to show Toast.
 
 export default class AccountManagerApexCall extends LightningElement {
     debugger;
@@ -31,8 +32,27 @@ export default class AccountManagerApexCall extends LightningElement {
                                                                                 // ('noOfAccounts' instead of 'this.noOfAccounts')
             console.log('response : '+JSON.stringify(response));
             this.accounts = response;
+
+            /* 
+                To show Toast on the screen, we have to first declare a 'ShowToastEvent' constructor and pass some parameters like 'title', 'message', 'variant'.
+                Fire the toastEvent using 'dispatchEvent' method.
+            */
+           const toastEvent = new ShowToastEvent({
+               title : 'Accounts Loaded',
+               message : this.noOfAccounts + ': Accounts successfully fetched from server',
+               variant : 'success'
+           });
+           this.dispatchEvent(toastEvent);
+
         }).catch(error =>{
-            console.error('Error in getting the records: ',error.body.message);
+             console.error('Error in getting the records: '+error.body.message);
+
+            const toastEvent = new ShowToastEvent({
+                title : 'Error',
+                message : error.body.message,
+                variant : 'error'
+            });
+            this.dispatchEvent(toastEvent);
         })
 
     }
