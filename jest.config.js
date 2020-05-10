adcc0d4122b6d5e26bd18ab496c409252b4c093e
Shailendra <shailendrakmr471@gmail.com>
2020-05-10 22:25:04 +0530

    Unit Testing and Debugging(in both Chrome and VS Code) LWC(meetingRooms) using Jest Framework

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+// Copy the below code from 'https://github.com/salesforce/sfdx-lwc-jest'
+
+const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+module.exports = {
+    ...jestConfig,
+    // add any custom configurations here
+};
