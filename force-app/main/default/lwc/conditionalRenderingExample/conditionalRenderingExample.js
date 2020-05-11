2a4d28fe30af388b7b505361b4536d07b233d261
Shailendra <shailendrakmr471@gmail.com>
2020-05-11 19:08:14 +0530

    Deleting unwanted files

--- force-app/main/default/lwc/conditionalRenderingExample/conditionalRenderingExample.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, track } from 'lwc';
-
-export default class ConditionalRenderingExample extends LightningElement {
-    @track displayDiv = false;
-    @track cityList = ['Delhi', 'Mumbai', 'Kolkata', 'Chennai'];
-    showDivHandler(event){
-        this.displayDiv = event.target.checked;
-    }
-}
